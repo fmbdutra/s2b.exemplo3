a277118c927f3cb74a807190909596648538a6d1
Fabiano M. Dutra <fmbdutra@gmail.com>
2018-05-08 15:55:14 -0300

    Adjust pom.xml to junit params

--- exemplo03/src/test/java/pucrs/s2b20181/exemplo03/AppTest.java
+++ exemplo03/src/test/java/pucrs/s2b20181/exemplo03/AppTest.java
@@ -1,38 +1,19 @@
+/*
+ * Fabiano Dutra
+ * S2B 2018/1
+ */
+
 package pucrs.s2b20181.exemplo03;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertTrue;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import org.junit.Test;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+public class AppTest {
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	@Test
+	public void testApp() {
+		assertTrue(true);
+	}
+
 }
