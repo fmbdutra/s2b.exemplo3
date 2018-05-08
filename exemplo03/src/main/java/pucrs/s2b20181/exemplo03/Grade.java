bcbe57693eb7daa355ed4601207d526d88b3fe70
Fabiano M. Dutra <fmbdutra@gmail.com>
2018-05-08 16:28:18 -0300

    Add class Score and new tests

---
+++ exemplo03/src/main/java/pucrs/s2b20181/exemplo03/Grade.java
@@ -0,0 +1,28 @@
+/*
+ * Fabiano Dutra
+ * S2B 2018/1
+ * TDD exemple - Test Driven Development
+ */
+
+
+package pucrs.s2b20181.exemplo03;
+
+public class Grade {
+
+	public static String calc(double s1, double s2) {
+		
+		double score = (s1+s2)/2.0;
+		if (score >= 7.0) {
+			return "Approved";
+		}
+		else if (score >= 4.0) {
+			return "In Exam";
+		}
+		else {
+			return "Reproved";
+		}
+		
+			
+	}
+
+}
--- exemplo03/src/test/java/pucrs/s2b20181/exemplo03/AppTest.java
+++ exemplo03/src/test/java/pucrs/s2b20181/exemplo03/AppTest.java
@@ -1,19 +1,39 @@
 /*
  * Fabiano Dutra
  * S2B 2018/1
+ * TDD exemple - Test Driven Development
  */
 
 package pucrs.s2b20181.exemplo03;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class AppTest {
 
 	@Test
-	public void testApp() {
-		assertTrue(true);
+	public void testFiveAndFiveGivesExam() 
+	{
+		String expected = "In Exam";
+		String actual = Grade.calc(5.0, 5.0);
+		assertEquals(expected, actual);		
+	}
+	
+	@Test
+	public void testSevenAndSevenGivesAproved() 
+	{
+		String expected = "Approved";
+		String actual = Grade.calc(7.0, 7.0);
+		assertEquals(expected, actual);		
+	}
+	
+	@Test
+	public void testThreeAndThreeGivesReproved() 
+	{
+		String expected = "Reproved";
+		String actual = Grade.calc(3.0, 3.0);
+		assertEquals(expected, actual);		
 	}
 
 }
