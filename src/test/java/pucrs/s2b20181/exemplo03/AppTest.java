a6a09613550c27f1b0c4fc866ea69dc637dc5505
Fabiano M. Dutra <38920482+fmbdutra@users.noreply.github.com>
2019-02-28 16:46:31 -0300

    ajust code format

--- src/test/java/pucrs/s2b20181/exemplo03/AppTest.java
+++ src/test/java/pucrs/s2b20181/exemplo03/AppTest.java
@@ -14,40 +14,35 @@ import org.junit.Test;
 public class AppTest {
 
 	@Test
-	public void testFiveAndFiveGivesExam() 
-	{
+	public void testFiveAndFiveGivesExam(){
 		String expected = "In Exam";
 		String actual = Grade.calc(5.0, 5.0);
 		assertEquals(expected, actual);		
 	}
 	
 	@Test
-	public void testSevenAndSevenGivesAproved() 
-	{
+	public void testSevenAndSevenGivesAproved(){
 		String expected = "Approved";
 		String actual = Grade.calc(7.0, 7.0);
 		assertEquals(expected, actual);		
 	}
 	
 	@Test
-	public void testThreeAndThreeGivesReproved() 
-	{
+	public void testThreeAndThreeGivesReproved() {
 		String expected = "Reproved";
 		String actual = Grade.calc(3.0, 3.0);
 		assertEquals(expected, actual);		
 	}
 	
 	@Test
-	public void testThree99AndThree99GivesReproved() 
-	{
+	public void testThree99AndThree99GivesReproved() {
 		String expected = "Reproved";
 		String actual = Grade.calc(3.999, 3.999);
 		assertEquals(expected, actual);		
 	}
 	
 	@Test
-	public void testThree9999AndThree0001GivesApproved() 
-	{
+	public void testThree9999AndThree0001GivesApproved() {
 		String expected = "In Exam";
 		String actual = Grade.calc(3.9999, 4.0001);
 		assertEquals(expected, actual);		
